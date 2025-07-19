fadf5f1ff177d6511cc361587f118c2175f44a2b
Ashik <110383734+ashik-maybe@users.noreply.github.com>
2025-07-19 12:56:22 +0600

    Update setup-dev-tools.sh

--- scripts/setup-dev-tools.sh
+++ scripts/setup-dev-tools.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Installs GitHub Desktop and Visual Studio Code on Ubuntu
+# Install GitHub Desktop and Visual Studio Code on Ubuntu
 
 #====================== LOGGING ==========================
 GREEN="\e[32m"; BLUE="\e[34m"; YELLOW="\e[33m"; RED="\e[31m"; RESET="\e[0m"
